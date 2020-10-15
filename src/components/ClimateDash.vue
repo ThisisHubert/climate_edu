fd45b01134e6d6ce0ee1ec5c49d3bf89e9c3440f
Hubert <hubertussetiawan911@gmail.com>
2020-10-15 22:50:07 +0900

    structure for apis

--- src/components/ClimateDash.vue
+++ src/components/ClimateDash.vue
@@ -1,46 +1,233 @@
 <template>
-  <div id="climatedash">
-    <p>This component’s code is in {{ filename }}</p>
-    <Content v-bind:weather_data="weather_data"></Content>
+  <div id="climateDash">
+    <div id="search">
+      <input
+        id="location-input"
+        type="text"
+        ref="input"
+        placeholder="Location?"
+        @keyup.enter="organizeAllDetails"
+      />
+      <button id="search-btn" @click="organizeAllDetails">
+        <img src="" width="24" height="24" />
+      </button>
+      <div id="info">
+        <div class="wrapper-left">
+          <div id="current-weather">
+            {{ currentWeather.temp }}
+            <span>°C</span>
+          </div>
+          <div id="weather-desc">{{ currentWeather.summary }}</div>
+          <div class="temp-max-min">
+            <div class="max-desc">
+              <div id="max-detail">
+                <i>▲</i>
+                {{ currentWeather.todayHighLow.todayTempHigh }}
+                <span>°C</span>
+              </div>
+              <div id="max-summary">
+                at {{ currentWeather.todayHighLow.todayTempHighTime }}
+              </div>
+            </div>
+            <div class="min-desc">
+              <div id="min-detail">
+                <i>▼</i>
+                {{ currentWeather.todayHighLow.todayTempLow }}
+                <span>°C</span>
+              </div>
+              <div id="min-summary">
+                at {{ currentWeather.todayHighLow.todayTempLowTime }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="wrapper-right">
+          <div class="date-time-info">
+            <div id="date-desc">
+              <img src="" width="20" height="20" />
+              {{ currentWeather.time }}
+            </div>
+          </div>
+          <div class="location-info">
+            <div id="location-desc">
+              <img src="" width="10.83" height="15.83" style="opacity: 0.9;" />
+              {{ currentWeather.full_location }}
+              <div id="location-detail" class="mt-1">
+                Lat: {{ currentWeather.formatted_lat }}
+                <br />
+                Long: {{ currentWeather.formatted_long }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <dashboard-content
+      :highlights="highlights"
+      :tempVar="tempVar"
+    ></dashboard-content>
   </div>
 </template>
 
 <script>
-import Content from './Content.vue'
+import Content from "./Content.vue";
 
 export default {
-  name: 'climatedash',
+  name: "climateDash",
   components: {
-    'Content': Content
+    "dashboard-content": Content,
   },
-  data () {
+  data() {
     return {
-      filename: 'App.vue',
-      weather_data: {
-        location: "California",
-        temperature: {
-          current: "35 C",
+      weatherDetails: false,
+      location: "", // raw location from input
+      lat: "", // raw latitude from google maps api response
+      long: "", // raw longitude from google maps api response
+      completeWeatherApi: "", // weather api string with lat and long
+      rawWeatherData: "", // raw response from weather api
+      currentWeather: {
+        full_location: "", // for full address
+        formatted_lat: "", // for N/S
+        formatted_long: "", // for E/W
+        time: "",
+        temp: "",
+        todayHighLow: {
+          todayTempHigh: "",
+          todayTempHighTime: "",
+          todayTempLow: "",
+          todayTempLowTime: "",
         },
-        highlights: {
-          uvindex: "3",
-          windstatus: {
-            speed: "20 km/h",
-            direction: "N-E",
-          },
-          visibility: "12 km",
+        summary: "",
+        possibility: "",
+      },
+      tempVar: {
+        tempToday: [
+          // gets added dynamically by this.getSetHourlyTempInfoToday()
+        ],
+      },
+      highlights: {
+        uvIndex: "",
+        visibility: "",
+        windStatus: {
+          windSpeed: "",
+          windDirection: "",
+          derivedWindDirection: "",
         },
       },
-    }
+    };
   },
+
   methods: {
-
+    convertToTitleCase: function(str) {
+      str = str.toLowerCase().split(" ");
+      for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+      }
+      return str.join(" ");
+    },
+    // To format the “possibility” (of weather) string obtained from the weather API
+    formatPossibility: function(str) {
+      str = str.toLowerCase().split("-");
+      for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+      }
+      return str.join(" ");
+    },
+    // To convert Unix timestamps according to our convenience
+    unixToHuman: function(timezone, timestamp) {
+      /* READ THIS BEFORE JUDGING & DEBUGGING
+     For any location beyond the arctic circle and the
+     antarctic circle, the goddamn weather api does not return certain
+     keys/values in each of this.rawWeatherData.daily.data[some_array_index].
+     Due to this, console throws up an error.
+     The code is correct, the problem is with the API.
+     May be later on I will add some padding to tackle missing values.
+     */
+      var moment = require("moment-timezone"); // for handling date & time
+      var decipher = new Date(timestamp * 1000);
+      var human = moment(decipher)
+        .tz(timezone)
+        .format("llll");
+      var timeArray = human.split(" ");
+      var timeNumeral = timeArray[4];
+      var timeSuffix = timeArray[5];
+      var justTime = timeNumeral + " " + timeSuffix;
+      var monthDateArray = human.split(",");
+      var monthDate = monthDateArray[1].trim();
+      return {
+        fullTime: human,
+        onlyTime: justTime,
+        onlyMonthDate: monthDate,
+      };
+    },
+    // To convert temperature from fahrenheit to celcius
+    fahToCel: function(tempInFahrenheit) {
+      var tempInCelcius = Math.round((5 / 9) * (tempInFahrenheit - 32));
+      return tempInCelcius;
+    },
+    // To convert the air pressure reading from millibar to kilopascal
+    milibarToKiloPascal: function(pressureInMilibar) {
+      var pressureInKPA = pressureInMilibar * 0.1;
+      return Math.round(pressureInKPA);
+    },
+    // To format the wind direction based on the angle
+    deriveWindDir: function(windDir) {
+      var wind_directions_array = [
+        { minVal: 0, maxVal: 30, direction: "N" },
+        { minVal: 31, maxVal: 45, direction: "NNE" },
+        { minVal: 46, maxVal: 75, direction: "NE" },
+        { minVal: 76, maxVal: 90, direction: "ENE" },
+        { minVal: 91, maxVal: 120, direction: "E" },
+        { minVal: 121, maxVal: 135, direction: "ESE" },
+        { minVal: 136, maxVal: 165, direction: "SE" },
+        { minVal: 166, maxVal: 180, direction: "SSE" },
+        { minVal: 181, maxVal: 210, direction: "S" },
+        { minVal: 211, maxVal: 225, direction: "SSW" },
+        { minVal: 226, maxVal: 255, direction: "SW" },
+        { minVal: 256, maxVal: 270, direction: "WSW" },
+        { minVal: 271, maxVal: 300, direction: "W" },
+        { minVal: 301, maxVal: 315, direction: "WNW" },
+        { minVal: 316, maxVal: 345, direction: "NW" },
+        { minVal: 346, maxVal: 360, direction: "NNW" },
+      ];
+      var wind_direction = "";
+      for (var i = 0; i < wind_directions_array.length; i++) {
+        if (
+          windDir >= wind_directions_array[i].minVal &&
+          windDir <= wind_directions_array[i].maxVal
+        ) {
+          wind_direction = wind_directions_array[i].direction;
+        }
+      }
+      return wind_direction;
+    },
+    makeInputEmpty: function() {
+      this.$refs.input.value = "";
+    },
+    makeTempVarTodayEmpty: function() {
+      this.tempVar.tempToday = [];
+    },
+    detectEnterKeyPress: function() {
+      var input = this.$refs.input;
+      input.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        var enterKeyCode = 13;
+        if (event.keyCode === enterKeyCode) {
+          this.setHitEnterKeyTrue();
+        }
+      });
+    },
+    // Continue from Action/event oriented methods
   },
-  computed: {
-
-  },
-}
+  computed: {},
+};
 </script>
 
 <style>
+  
+</style>>
 
-</style>
+
+
+
